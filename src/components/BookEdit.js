875cadeb632568768cb1e1cc9ff33f703e450a1d
Ziad Mahmoud <ziadmahmoudx98@gmail.com>
2024-02-21 22:49:15 +0200

    Books Shop Version 1 Ending

---
+++ src/components/BookEdit.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function BookEdit({ book, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  };
+
+  return (
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label htmlFor="edit">Title</label>
+      <input
+        value={title}
+        onChange={handleChange}
+        id="edit"
+        className="input"
+      />
+      <button className="button in-primary">Save!</button>
+    </form>
+  );
+}
+
+export default BookEdit;
---
+++ src/components/BookShow.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+function BookShow({ book, onDelete, onEdit }) {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSubmit = (id, newTitle) => {
+    onEdit(id, newTitle);
+    setShowEdit(false);
+  };
+  let content = <div> {book.title} </div>;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
+  }
+  return (
+    <div className="book-show">
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="photos" />
+      {content}
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BookShow;
