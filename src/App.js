f476d854ba96d6e4f4575f45732d3fe08fa1ad75
Ziad Mahmoud <ziadmahmoudx98@gmail.com>
2024-02-22 19:51:31 +0200

    fix useEffice bug and using useCallBack The app is done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
   //Reading DATA
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get('http://localhost:3001/books');
     setBooks(response.data);
-  };
+  }, []);
   useEffect(() => {
     fetchBooks();
-  }, []);
+  }, [fetchBooks]);
   // Create DATA {BOOKS}
   const creatBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', {
@@ -35,7 +35,7 @@ function App() {
     const response = await axios.put(`http://localhost:3001/books/${id}`, {
       title: newTitle
     });
-    console.log(response.data);
+
     const updatedBooks = books.map((book) => {
       if (id === book.id) {
         return { ...book, ...response.data };
